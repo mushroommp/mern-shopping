1f0d2aa7947769786c2eb6ca1d924477950d720c
mushroommp <143livinlife@gmail.com>
2020-08-30 17:44:56 +0800

    Edit gitignore

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AppNavBar from './components/AppNavbar'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Test</h1>
+      <AppNavBar />
+    </div>
+  );
+}
+
+export default App;
